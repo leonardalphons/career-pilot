1f0a19264cbd5858ebc65b20d6346afbde2d2585
Leonard <leonardalphons@gmail.com>
2026-07-23 22:43:36 +0530

    added mongo uri

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 async function connectToDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("Connected to Database");
+    console.log("Connected to Database...");
   } catch (err) {
     console.log(err);
   }
